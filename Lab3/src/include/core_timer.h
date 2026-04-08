4b06f4d107c80a556dd74f787fae84485b816691
RichardLin8331 <leo0007400074@gmail.com>
2026-04-08 11:00:45 +0800

    feat: Implement EL0 system call handler

---
+++ Lab3/src/include/core_timer.h
@@ -0,0 +1,7 @@
+#ifndef R_CORE_TIMER_H
+#define R_CORE_TIMER_H
+void core_timer_enable();
+void core_timer_handler();
+
+
+#endif
--- Lab3/src/kernel.c
+++ Lab3/src/kernel.c
@@ -3,7 +3,17 @@
 #include "include/shell.h"
 void main () {
     uart_init();
+    
     uart_send_string("----- NYCU OSDI LAB 3 -----\r\n");
+    /*
+    unsigned long el = 0;
+    asm volatile("mrs %0, CurrentEL" : "=r" (el));
+    el = (el >> 2) & 3;
+    char el_string[20] = "0x00000000";
+    htos(el, el_string);
+    uart_send_string("Current EL is ");
+    uart_send_string(el_string);
+    uart_send_string("\r\n");*/
     
     char s0[] = "# Hello !!\r\n# Finish Booting !!\r\n"; 
     uart_send_string(s0);
