513c7ccac2c68341e7fa1310e75ced56704dfe44
RichardLin8331 <leo0007400074@gmail.com>
2026-04-08 20:13:54 +0800

    fix: handle specific interrupt source in branch

---
+++ Lab3/src/include/irq_handler.h
@@ -0,0 +1,6 @@
+#ifndef R_IRQ_HANDLER_H
+#define R_IRQ_HANDLER_H
+
+#define CORE0INTSRC ((unsigned int*)0x40000060)
+
+#endif
--- Lab3/src/irq_handler.c
+++ Lab3/src/irq_handler.c
@@ -1,14 +1,21 @@
 #include "include/uart.h"
 #include "include/core_timer.h"
 #include "include/string.h"
+#include "include/irq_handler.h"
+
 int core_timer_cnt = 0;
 void irq_handler() {
-    core_timer_cnt++;
-    uart_send_string("Core timer triggered.\r\n# ");
-    uart_send_string("Jiffies: ");
-    char timer_cnt_string[3];
-    itos(core_timer_cnt, timer_cnt_string);
-    uart_send_string(timer_cnt_string);
-    uart_send_string("\r\n# ");
-    core_timer_handler();
+    unsigned int core0int = 0;
+    core0int = *CORE0INTSRC;
+    if (core0int & 0x2) {
+        core_timer_cnt++;
+        uart_send_string("Core timer triggered.\r\n# ");
+        uart_send_string("Jiffies: ");
+        char timer_cnt_string[3];
+        itos(core_timer_cnt, timer_cnt_string);
+        uart_send_string(timer_cnt_string);
+        uart_send_string("\r\n# ");
+        core_timer_handler();
+    }
+    
 }
