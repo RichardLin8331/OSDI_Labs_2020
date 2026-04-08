e43dd50805597008a6b41beed39f707a7a98a944
RichardLin8331 <leo0007400074@gmail.com>
2026-04-08 21:29:41 +0800

    feat: implement ring buffer for uart interrupt

--- Lab3/src/include/uart.h
+++ Lab3/src/include/uart.h
@@ -2,6 +2,7 @@
 #define R_UART_H
 
 #include "gpio.h"
+#include "queue.h"
 
 #define UART0_DR        ((volatile unsigned int*)(MEM_BASE+0x00201000))
 #define UART0_FR        ((volatile unsigned int*)(MEM_BASE+0x00201018))
@@ -10,11 +11,20 @@
 #define UART0_LCRH      ((volatile unsigned int*)(MEM_BASE+0x0020102C))
 #define UART0_CR        ((volatile unsigned int*)(MEM_BASE+0x00201030))
 #define UART0_IMSC      ((volatile unsigned int*)(MEM_BASE+0x00201038))
+#define UART0_MIS       ((volatile unsigned int*)(MEM_BASE+0x00201040))
 #define UART0_ICR       ((volatile unsigned int*)(MEM_BASE+0x00201044))
+
+
+
+char sender_queue[QUEUE_SIZE];
+char receiver_queue[QUEUE_SIZE];
+
+int sq_head, sq_tail, rq_head, rq_tail;
+
 
 void uart_init();
 void uart_send(unsigned int c);
 char uart_get();
 void uart_send_string(char* s);
-
+void uart_IRQ_handler() ;
 #endif
---
+++ Lab3/src/queue.c
@@ -0,0 +1,21 @@
+#include "include/queue.h"
+
+int queue_empty(int q_head, int q_tail) {
+    return (q_head == q_tail)? 1 : 0;
+}
+
+int queue_full(int q_head, int q_tail) {
+    if ((q_tail + 1) % QUEUE_SIZE == q_head) return 1;
+    return 0;
+}
+
+void queue_push(char* q, int* q_tail, char c) {
+    q[*q_tail] = c;
+    *(++q_tail) %= QUEUE_SIZE;
+}
+
+char queue_pop(char* q, int* q_head) {
+    char tmp = q[*q_head];
+    *(++q_head) %= QUEUE_SIZE;
+    return tmp;
+}
